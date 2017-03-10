333f831f525c06f3c4caa56d05fa4293d0f22634
puan0601 <anton.pugach@gmail.com>
2017-03-10 14:29:13 -0800

    index and create

---
+++ lib-app/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    @user = User.create(user_params)
+
+    redirect_to root_path
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
+end
---
+++ lib-app/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root to: 'users#index'
+
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users/', to: "users#create"
+  get '/users/:id', to: 'users#show'
+end
