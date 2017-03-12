fc689c2f8bd0a74ca28e34a4b0f3b680d5824564
puan0601 <anton.pugach@gmail.com>
2017-03-12 15:00:37 -0700

    update user_params

--- lib-app/app/controllers/users_controller.rb
+++ lib-app/app/controllers/users_controller.rb
@@ -9,14 +9,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
     @user = User.create(user_params)
-
     redirect_to root_path
   end
 
   def show
     @user = User.find(params[:id])
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 end
