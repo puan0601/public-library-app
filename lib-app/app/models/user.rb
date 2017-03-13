26265ea75ad7259791b9f8c7281a9feb7d46f9c9
puan0601 <anton.pugach@gmail.com>
2017-03-13 02:15:20 -0700

    created user library relations and join table

--- lib-app/app/models/user.rb
+++ lib-app/app/models/user.rb
@@ -13,6 +13,8 @@
 
 class User < ApplicationRecord
   has_secure_password
+  has_many :library_users, dependent: :destroy
+  has_many :libraries, through: :library_users
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
