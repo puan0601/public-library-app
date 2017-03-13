436784d380a66f4b617d5dec852469426158c8a5
puan0601 <anton.pugach@gmail.com>
2017-03-13 01:41:09 -0700

    user show works due to bug fix. added flash messaging

--- lib-app/config/routes.rb
+++ lib-app/config/routes.rb
@@ -1,10 +1,24 @@
+# == Route Map
+#
+#   Prefix Verb URI Pattern          Controller#Action
+#     root GET  /                    users#index
+# new_user GET  /users/new(.:format) users#new
+#    users POST /users(.:format)     users#create
+#          GET  /users/:id(.:format) users#show
+#    login GET  /login(.:format)     sessions#new
+#   logout GET  /logout(.:format)    sessions#destroy
+# sessions POST /sessions(.:format)  sessions#create
+#
+
 Rails.application.routes.draw do
   root to: 'users#index'
 
   get '/users/new', to: 'users#new', as: 'new_user'
   post '/users/', to: "users#create"
-  get '/users/:id', to: 'users#show'
+  get '/users/:id', to: 'users#show', as: 'user'
 
   get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
   post '/sessions', to: 'sessions#create'
+
 end
