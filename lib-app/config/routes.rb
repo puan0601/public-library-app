9a770a2ece2a808838679c0b90866717ed7a5164
puan0601 <anton.pugach@gmail.com>
2017-03-13 00:59:32 -0700

    login new

--- lib-app/config/routes.rb
+++ lib-app/config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get '/users/new', to: 'users#new', as: 'new_user'
   post '/users/', to: "users#create"
   get '/users/:id', to: 'users#show'
+
+  get '/login', to: 'sessions#new'
+
 end
